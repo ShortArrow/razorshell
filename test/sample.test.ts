7a2302a0667ea83cedf3a84d9c7bbb8515de4be6
ShortArrow <bamboogeneral@shortarrow.jp>
2024-07-09 05:19:42 +0900

    add cursor control todo

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -4,6 +4,7 @@ import { cursor } from "../src/cursor";
 test("moveCursorToBeginning", () => {
   const target = "hello world new order";
   const actual = cursor.getTopOfWord(target, 9);
+  // todo update input arg current index
   expect(actual).toBe(7);
 });
 test("split", () => {
