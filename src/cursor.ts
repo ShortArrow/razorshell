8de6ea44c98209f0bc4f98f8124e29e63cf4db65
ShortArrow <bamboogeneral@shortarrow.jp>
2024-08-03 19:31:00 +0900

    update moveCursorToBeginning

--- src/cursor.ts
+++ src/cursor.ts
@@ -1,6 +1,15 @@
 export const cursor = {
   getTopOfWord(text: string, cursor: number): number {
-    const splited = text.slice(cursor).split(" ");
-    return cursor - splited[splited.length - 1].length + 2;
+    const splited = text.slice(undefined, cursor).split(" ");
+    let wordcount = 0;
+    let charcount = 0;
+    while (splited[wordcount]) {
+      charcount += splited[wordcount].length + 1;
+      if (charcount + splited[wordcount].length >= cursor) {
+        break;
+      }
+      wordcount++;
+    }
+    return charcount + 1;
   },
 };
--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,11 +1,25 @@
 import { expect, test } from "vitest";
 import { cursor } from "../src/cursor";
 
-test("moveCursorToBeginning", () => {
+test("moveCursorToBeginning of world", () => {
   const target = "hello world new order";
   const actual = cursor.getTopOfWord(target, 9);
-  // todo update input arg current index
   expect(actual).toBe(7);
+});
+test("moveCursorToBeginning of new", () => {
+  const target = "hello world new order";
+  const actual = cursor.getTopOfWord(target, 14);
+  expect(actual).toBe(13);
+});
+test("moveCursorToBeginning of new", () => {
+  const target = "hello world new order";
+  const actual = cursor.getTopOfWord(target, 13);
+  expect(actual).toBe(13);
+});
+test("moveCursorToBeginning of new", () => {
+  const target = "hello world new order";
+  const actual = cursor.getTopOfWord(target, 15);
+  expect(actual).toBe(13);
 });
 test("split", () => {
   const target = "hello world new order";
