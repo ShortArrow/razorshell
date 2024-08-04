916fe5b44eac6711336bc4bb2b5b853f73e5803a
ShortArrow <bamboogeneral@shortarrow.jp>
2024-08-04 17:29:12 +0900

    update get top of word

--- src/cursor.ts
+++ src/cursor.ts
@@ -3,6 +3,9 @@ export const cursor = {
     const splited = text.slice(undefined, cursor).split(" ");
     let wordcount = 0;
     let charcount = 0;
+    if (splited[wordcount].length + 2 > cursor) {
+      return 1
+    }
     while (splited[wordcount]) {
       charcount += splited[wordcount].length + 1;
       if (charcount + splited[wordcount].length >= cursor) {
--- test/sample.test.ts
+++ test/sample.test.ts
@@ -6,20 +6,45 @@ test("moveCursorToBeginning of world", () => {
   const actual = cursor.getTopOfWord(target, 9);
   expect(actual).toBe(7);
 });
-test("moveCursorToBeginning of new", () => {
+test("hello world ne", () => {
   const target = "hello world new order";
   const actual = cursor.getTopOfWord(target, 14);
   expect(actual).toBe(13);
 });
-test("moveCursorToBeginning of new", () => {
+test("hello world n", () => {
   const target = "hello world new order";
   const actual = cursor.getTopOfWord(target, 13);
   expect(actual).toBe(13);
 });
-test("moveCursorToBeginning of new", () => {
+test("hello world new", () => {
   const target = "hello world new order";
   const actual = cursor.getTopOfWord(target, 15);
   expect(actual).toBe(13);
+});
+test("h", () => {
+  const target = "hello world new order";
+  const actual = cursor.getTopOfWord(target, 1);
+  expect(actual).toBe(1);
+});
+test("_", () => {
+  const target = "hello world new order";
+  const actual = cursor.getTopOfWord(target, 0);
+  expect(actual).toBe(1);
+});
+test("hello", () => {
+  const target = "hello world new order";
+  const actual = cursor.getTopOfWord(target, 5);
+  expect(actual).toBe(1);
+});
+test("hello_", () => {
+  const target = "hello world new order";
+  const actual = cursor.getTopOfWord(target, 6);
+  expect(actual).toBe(1);
+});
+test("hello w", () => {
+  const target = "hello world new order";
+  const actual = cursor.getTopOfWord(target, 7);
+  expect(actual).toBe(7);
 });
 test("split", () => {
   const target = "hello world new order";
