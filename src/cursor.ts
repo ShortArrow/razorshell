10d750a7695592f9faf1e72c07ab65f0014bdea5
ShortArrow <bamboogeneral@shortarrow.jp>
2024-09-01 10:47:49 +0900

    update feature get top of word

--- src/cursor.ts
+++ src/cursor.ts
@@ -1,19 +1,8 @@
 export const cursor = {
   getTopOfWord(text: string, cursor: number): number {
-    const splited = text.slice(undefined, cursor).split(" ");
-    let wordcount = 0;
-    let charcount = 0;
-    if (splited[wordcount].length + 2 > cursor) {
-      return 1;
-    }
-    while (splited[wordcount]) {
-      charcount += splited[wordcount].length + 1;
-      if (charcount + splited[wordcount].length >= cursor) {
-        break;
-      }
-      wordcount++;
-    }
-    return charcount;
+    const sliced = text.slice(undefined, cursor);
+    const splited = sliced.split(" ");
+    return cursor - splited.reverse()[0].length;
   },
   getEndOfWord(text: string, cursor: number): number {
     const sliced = text.slice(cursor);
--- test/topofword.test.ts
+++ test/topofword.test.ts
@@ -4,47 +4,47 @@ import { cursor } from "../src/cursor";
 test("moveCursorToBeginning of world", () => {
   const target = "hello world new order";
   const actual = cursor.getTopOfWord(target, 9);
-  expect(actual).toBe(7);
+  expect(actual).toBe(6);
 });
 test("hello world ne", () => {
   const target = "hello world new order";
   const actual = cursor.getTopOfWord(target, 14);
-  expect(actual).toBe(13);
+  expect(actual).toBe(12);
 });
 test("hello world n", () => {
   const target = "hello world new order";
   const actual = cursor.getTopOfWord(target, 13);
-  expect(actual).toBe(13);
+  expect(actual).toBe(12);
 });
 test("hello world new", () => {
   const target = "hello world new order";
   const actual = cursor.getTopOfWord(target, 15);
-  expect(actual).toBe(13);
+  expect(actual).toBe(12);
 });
 test("h", () => {
   const target = "hello world new order";
   const actual = cursor.getTopOfWord(target, 1);
-  expect(actual).toBe(1);
+  expect(actual).toBe(0);
 });
 test("_", () => {
   const target = "hello world new order";
   const actual = cursor.getTopOfWord(target, 0);
-  expect(actual).toBe(1);
+  expect(actual).toBe(0);
 });
 test("hello", () => {
   const target = "hello world new order";
   const actual = cursor.getTopOfWord(target, 5);
-  expect(actual).toBe(1);
+  expect(actual).toBe(0);
 });
 test("hello_", () => {
   const target = "hello world new order";
   const actual = cursor.getTopOfWord(target, 6);
-  expect(actual).toBe(1);
+  expect(actual).toBe(6);
 });
 test("hello w", () => {
   const target = "hello world new order";
   const actual = cursor.getTopOfWord(target, 7);
-  expect(actual).toBe(7);
+  expect(actual).toBe(6);
 });
 test("split", () => {
   const target = "hello world new order";
