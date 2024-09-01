f7457278d64966509006a4a0b1db9345ec158b56
ShortArrow <bamboogeneral@shortarrow.jp>
2024-09-01 10:05:26 +0900

    update feature get end of word

--- src/cursor.ts
+++ src/cursor.ts
@@ -4,7 +4,7 @@ export const cursor = {
     let wordcount = 0;
     let charcount = 0;
     if (splited[wordcount].length + 2 > cursor) {
-      return 1
+      return 1;
     }
     while (splited[wordcount]) {
       charcount += splited[wordcount].length + 1;
@@ -14,5 +14,18 @@ export const cursor = {
       wordcount++;
     }
     return charcount;
+  },
+  getEndOfWord(text: string, cursor: number): number {
+    const splited = text.slice(cursor).split(" ");
+    let wordcount = 0;
+    let charcount = cursor;
+    while (splited[wordcount]) {
+      charcount += splited[wordcount].length + 1;
+      if (charcount + splited[wordcount].length >= cursor) {
+        break;
+      }
+      wordcount++;
+    }
+    return charcount + splited[wordcount].length;
   },
 };
---
+++ test/endofword.test.ts
@@ -0,0 +1,53 @@
+import { expect, test } from "vitest";
+import { cursor } from "../src/cursor";
+
+test("moveCursorToBeginning of world", () => {
+  const target = "hello world new order";
+  const actual = cursor.getEndOfWord(target, 9);
+  expect(actual).toBe(11);
+});
+test("hello world ne", () => {
+  const target = "hello world new order";
+  const actual = cursor.getEndOfWord(target, 14);
+  expect(actual).toBe(15);
+});
+test("hello world n", () => {
+  const target = "hello world new order";
+  const actual = cursor.getEndOfWord(target, 13);
+  expect(actual).toBe(15);
+});
+test("hello world new", () => {
+  const target = "hello world new order";
+  const actual = cursor.getEndOfWord(target, 15);
+  expect(actual).toBe(15);
+});
+test("h", () => {
+  const target = "hello world new order";
+  const actual = cursor.getEndOfWord(target, 1);
+  expect(actual).toBe(5);
+});
+test("_", () => {
+  const target = "hello world new order";
+  const actual = cursor.getEndOfWord(target, 0);
+  expect(actual).toBe(5);
+});
+test("hello", () => {
+  const target = "hello world new order";
+  const actual = cursor.getEndOfWord(target, 5);
+  expect(actual).toBe(11);
+});
+test("hello_", () => {
+  const target = "hello world new order";
+  const actual = cursor.getEndOfWord(target, 6);
+  expect(actual).toBe(11);
+});
+test("hello w", () => {
+  const target = "hello world new order";
+  const actual = cursor.getEndOfWord(target, 7);
+  expect(actual).toBe(11);
+});
+test("split", () => {
+  const target = "hello world new order";
+  const actual = target.slice(0, 9);
+  expect(actual).toEqual("hello wor");
+});
