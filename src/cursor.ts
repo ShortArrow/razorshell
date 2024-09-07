ffb2b42b17badfb2700bbca5c1f51b805d6dd6c2
ShortArrow <bamboogeneral@shortarrow.jp>
2024-09-07 19:56:35 +0900

    refactor word end and top to semantic

--- src/cursor.ts
+++ src/cursor.ts
@@ -2,19 +2,24 @@ export const cursor = {
   getTopOfWord(text: string, cursor: number): number {
     if (cursor === text.length + 1) return cursor - 1;
     if (cursor === 0) return 0;
-    const sliced = text.slice(undefined, cursor);
-    const splited = sliced.split(" ");
-    splited.reverse();
-    const responce = cursor - splited[0].length;
-    if (responce === cursor) return cursor - splited.reverse()[1].length - 1;
-    return responce;
+    const sliced = text.slice(0, cursor);
+    const words = sliced.split(" ");
+    const lastWordLength = words[words.length - 1].length;
+    const newCursor = cursor - lastWordLength;
+    if (newCursor === cursor) {
+      return cursor - words[words.length - 1].length - 1;
+    }
+    return newCursor;
   },
   getEndOfWord(text: string, cursor: number): number {
     const sliced = text.slice(cursor);
     if (sliced === "") return text.length;
-    const splited = sliced.split(" ");
-    const responce = cursor + splited[0].length;
-    if (responce === cursor) return cursor + splited[1].length + 1;
-    return responce;
+    const words = sliced.split(" ");
+    const firstWordLength = words[0].length;
+    const newCursor = cursor + firstWordLength;
+    if (newCursor === cursor) {
+      return cursor + words[1].length + 1;
+    }
+    return newCursor;
   },
 };
