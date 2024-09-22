b8741a1e5565c9712e78945fdf65f8f58d4d69ee
ShortArrow <bamboogeneral@shortarrow.jp>
2024-09-23 01:53:28 +0900

    Refactor OptionsUI component to add footer functionality

--- src/options-ui.tsx
+++ src/options-ui.tsx
@@ -3,17 +3,22 @@ import { LangApp } from './ui/language';
 import { TitleApp } from './ui/title';
 import { TestApp } from './ui/testui';
 import { ThemeApp } from './ui/theme';
+import { FooterApp } from './ui/footer';
 
 function OptionsUI() {
 
   return (
     <>
-      <div className='prose flex flex-col p-6 gap-6 container mx-auto h-dvh'>
-        <ThemeApp />
-        <TitleApp />
-        <TestApp />
-        <UrlApp />
-        <LangApp />
+      <div className='h-dvh flex flex-col'>
+        <div className='prose flex flex-col p-6 gap-6 mx-auto grow'>
+          <ThemeApp />
+          <TitleApp />
+          <TestApp />
+          <UrlApp />
+          <LangApp />
+        </div>
+        <div className='grow'></div>
+        <FooterApp />
       </div>
     </>
   );
