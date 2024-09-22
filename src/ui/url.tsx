6882cf7522e119ca4e48198e9b54adc3389998f9
ShortArrow <bamboogeneral@shortarrow.jp>
2024-09-22 23:40:21 +0900

    Refactor URL component to display a list of clickable URLs

--- src/ui/url.tsx
+++ src/ui/url.tsx
@@ -12,23 +12,30 @@ export function UrlApp() {
   }, []);
 
   return <>
-    <div className='flex flex-col'>
+    <div className='flex flex-col w-full'>
       <h2 className='h2'>URL list</h2>
-      <div>
-        {
-          urls.map((url, index) => {
-            return <div key={index}>{url}</div>;
-          })
-        }
-      </div>
-      <label className='input input-bordered flex p-0'>
-        <input type="url" id="url" className='grow' />
-        <button className='btn btn-neutral' onClick={() => {
+      <div className='join w-full'>
+        <label className='input input-bordered join-item flex justify-center items-center grow'>
+          <input type="url" id="url" className='grow' />
+        </label>
+        <button className='btn btn-neutral join-item' onClick={() => {
           const url = (document.getElementById('url') as HTMLInputElement).value;
           saveUrl([url]);
           setUrls([...urls, url]);
         }}>add url</button>
-      </label>
+      </div>
+      <div>
+        <ul>
+          {
+            urls.map((url, index) => {
+              if (url === '') return null;
+              return <li key={index}>
+                <a href={url}>{url}</a>
+              </li>;
+            })
+          }
+        </ul>
+      </div>
     </div>
   </>;
 }
