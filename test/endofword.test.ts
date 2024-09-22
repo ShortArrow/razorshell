5497afba981025beef132700a4163f9a05fe927c
ShortArrow <bamboogeneral@shortarrow.jp>
2024-09-22 13:58:25 +0900

    Refactor cursor module to improve word navigation logic

--- test/endofword.test.ts
+++ test/endofword.test.ts
@@ -12,28 +12,34 @@ describe("cursor.getEndOfWord", () => {
   test(target.slice(undefined,9), () => {
     expect(actual(9)).toBe(11);
   });
-  test("hello world ne", () => {
-    expect(actual(14)).toBe(15);
+  test("hello world_", () => {
+    expect(actual(11)).toBe(15);
   });
   test("hello world n", () => {
+    expect(actual(12)).toBe(15);
+  });
+  test("hello world ne", () => {
     expect(actual(13)).toBe(15);
   });
   test("hello world new", () => {
+    expect(actual(14)).toBe(15);
+  });
+  test("hello world new_", () => {
     expect(actual(15)).toBe(21);
   });
-  test("h", () => {
+  test("he", () => {
     expect(actual(1)).toBe(5);
   });
-  test("_", () => {
+  test("h", () => {
     expect(actual(0)).toBe(5);
   });
-  test("hello", () => {
+  test("hello_", () => {
     expect(actual(5)).toBe(11);
   });
-  test("hello_", () => {
+  test("hello w", () => {
     expect(actual(6)).toBe(11);
   });
-  test("hello w", () => {
+  test("hello wo", () => {
     expect(actual(7)).toBe(11);
   });
   test("split", () => {
